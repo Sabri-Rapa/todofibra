399349a13952d5000c41a463a54527e9cf0325fb
Sabri-Rapa <sabrina.rapa@hotmail.com>
2025-09-29 10:54:47 -0300

    fix: import images

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
+  const logohorizontal = "/images/logo/logohorizontal.png";
   return (
     <header className="sticky top-0 z-50 bg-light shadow-sm h-20 w-full">
       <nav className="w-full flex justify-center h-full">
         <div className="w-full max-w-[1300px] flex items-center justify-between">
           {/* Logo */}
-          <Image
-            src="/images/logo/logohorizontal.png"
+          <img
+            src={logohorizontal}
             alt="Logo"
             width={150}
             height={50}
